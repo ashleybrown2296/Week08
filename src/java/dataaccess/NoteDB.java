3b0c80500480b403be9abd1be886583f0e59e1f0
679918 <679918@ELF6IT215951.ACDM.DS.SAIT.CA>
2017-11-01 09:54:43 -0600

    Week08Lab- Complete Week08Lab

---
+++ src/java/dataaccess/NoteDB.java
@@ -0,0 +1,108 @@
+package dataaccess;
+
+import domainmodel.Note;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+public class NoteDB {
+
+    public int insert(Note note) throws NotesDBException {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        
+        try {
+            trans.begin();
+            em.persist(note);
+            trans.commit();
+            return 1;
+        } catch (Exception ex) {
+            trans.rollback();
+            Logger.getLogger(NoteDB.class.getName()).log(Level.SEVERE, "Cannot insert " + note.toString(), ex);
+            throw new NotesDBException("Error inserting note");
+        } finally {
+            em.close();
+        }
+    }
+
+    public int update(Note note) throws NotesDBException {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        
+        try {
+            trans.begin();
+            em.merge(note);
+            trans.commit();
+            return 1;
+        } catch (Exception ex) {
+            trans.rollback();
+            Logger.getLogger(NoteDB.class.getName()).log(Level.SEVERE, "Cannot update " + note.toString(), ex);
+            throw new NotesDBException("Error updating note");
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Note> getAll() throws NotesDBException {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        try {
+            List<Note> notes = em.createNamedQuery("User.findAll", Note.class).getResultList();
+            return notes;
+        } catch (Exception ex) {
+            Logger.getLogger(NoteDB.class.getName()).log(Level.SEVERE, "Cannot read notes", ex);
+            throw new NotesDBException("Error getting Notes");
+        } finally {
+            em.close();
+        }
+    }
+
+    /**
+     * Get a single user by their username.
+     *
+     * @param username The unique username.
+     * @return A User object if found, null otherwise.
+     * @throws NotesDBException
+     */
+    public Note getUser(String username) throws NotesDBException {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        
+        try {
+            Note user = em.find(Note.class, username);
+            return user;
+        } catch (Exception ex) {
+            Logger.getLogger(NoteDB.class.getName()).log(Level.SEVERE, "Cannot read notes", ex);
+            throw new NotesDBException("Error getting Notes");
+        } finally {
+            em.close();
+        }
+    }
+
+    public int delete(Note note) throws NotesDBException {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        
+        try {
+            trans.begin();
+            em.remove(em.merge(note));
+            trans.commit();
+            return 1;
+        } catch (Exception ex) {
+            trans.rollback();
+            Logger.getLogger(NoteDB.class.getName()).log(Level.SEVERE, "Cannot delete " + note.toString(), ex);
+            throw new NotesDBException("Error deleting note");
+        } finally {
+            em.close();
+        }
+    }
+
+    public Note getNote(int noteId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
